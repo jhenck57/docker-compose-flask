0aa5270766180756fd0251bab7de1600a9b5afaf
John Hencken <jhenck57@scarletmail.rutgers.edu>
2019-08-27 11:18:52 -0400

    still trying to modulate

--- web/app/translate.py
+++ web/app/translate.py
@@ -8,7 +8,7 @@ def translate(text, source_language, dest_language):
     if 'MS_TRANSLATOR_KEY' not in app.config or \
             not app.config['MS_TRANSLATOR_KEY']:
         return _('Error: the translation service is not configured.')
-    auth = {'Ocp-Apim-Subscription-Key': app.config['MS_TRANSLATOR_KEY']}
+    auth = {'5b7be27703094a34a4adbb7f659778b2': app.config['MS_TRANSLATOR_KEY']}
     r = requests.get('https://api.microsofttranslator.com/v2/Ajax.svc'
                      '/Translate?text={}&from={}&to={}'.format(
                          text, source_language, dest_language),
